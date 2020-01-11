5df88e2708c2fee38948e4802c74b042e0332466
FirosStuart <35224826+FirosStuart@users.noreply.github.com>
2020-01-11 14:09:04 +0900

    Update framework.h

--- PuzzleChatGame/framework.h
+++ PuzzleChatGame/framework.h
@@ -4,6 +4,9 @@
 
 #pragma once
 
+// 使用ライブラリ
+#pragma comment(lib,"wsock32.lib")
+
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
@@ -13,3 +16,8 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+#include <Windows.h>
+#include <WinSock.h>
+#include <string>
+#include <stdio.h>
+#include <ctime>

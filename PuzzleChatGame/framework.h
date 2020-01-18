6a815facf6ecd6c324df2dc92705fd8ae6a67032
FirosStuart <35224826+FirosStuart@users.noreply.github.com>
2020-01-18 11:42:50 +0900

    Add files via upload

---
+++ PuzzleChatGame/framework.h
@@ -0,0 +1,23 @@
+﻿// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+// 使用ライブラリ
+#pragma comment(lib,"wsock32.lib")
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <WinSock.h>
+#include <string>
+#include <stdio.h>
+#include <ctime>
---
+++ README.md
@@ -0,0 +1,8 @@
+Project ChatPuzzle Game SoftWare
+Version 1.7.15
+Copyright(c) 2019
+
+
+
+
+
